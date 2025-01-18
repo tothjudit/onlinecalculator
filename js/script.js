5c76318641c25d9baa5a59f1c5b4736588d5f8ca
Toth Judit <tothjudit.ditta@gmail.com>
2025-01-18 23:05:55 +0000

    Added JS Calculator class with basic arithmetic methods

---
+++ js/script.js
@@ -0,0 +1,15 @@
+class Calculator {
+    add(a, b) {
+        return a + b;
+    }
+    subtract(a, b) {
+        return a - b;
+    }
+    multiply(a, b) {
+        return a * b;
+    }
+    divide(a, b) {
+        if (b === 0) return 'Error';
+        return a / b;
+    }
+}
